1998a668f6a89465cb4f583866220074c0767782
Fernando Escolar <f.escolar@hotmail.com>
2021-11-10 20:40:30 +0100

    adding some styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './styles/main.scss';
+
+/* navbar opacity management */
+window.addEventListener('scroll', function(e) {
+  if (window.scrollY > 100) {
+    document.getElementsByTagName("nav")[0].classList.add("solid");
+  } else {
+    document.getElementsByTagName("nav")[0].classList.remove("solid");
+  }
+});
+
+/* hamburger status management: opened or closed */
+document.getElementById('hamburger').addEventListener('click', function () {
+  const classList = document.getElementsByTagName("nav")[0].classList;
+  if (classList.contains('active')) {
+    classList.remove('active');
+  } else {
+    classList.add('active');
+  }
+});
